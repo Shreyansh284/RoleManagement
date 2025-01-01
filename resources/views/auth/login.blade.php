7d0358e10689069af13a6e27b4d61c0fa21af551
Shreyansh284 <shreyanshranpariya@gmail.com>
2025-01-01 17:10:53 +0530

    added progressive web app feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,10 @@
 
   <!-- Template Main CSS File -->
   <link href="{{url('')}}/assets/css/style.css" rel="stylesheet">
-
+<!-- PWA  -->
+<meta name="theme-color" content="#6777ef"/>
+<link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
 
 </head>
 
@@ -110,7 +113,23 @@
 
   <!-- Template Main JS File -->
   <script src="{{url('')}}/assets/js/main.js"></script>
-
+  <script src="{{ asset('/sw.js') }}"></script>
+  <script>
+     if ("serviceWorker" in navigator) {
+        // Register a service worker hosted at the root of the
+        // site using the default scope.
+        navigator.serviceWorker.register("/sw.js").then(
+        (registration) => {
+           console.log("Service worker registration succeeded:", registration);
+        },
+        (error) => {
+           console.error(`Service worker registration failed: ${error}`);
+        },
+      );
+    } else {
+       console.error("Service workers are not supported.");
+    }
+  </script>
 </body>
 
 </html>
--- resources/views/panel/layouts/app.blade.php
+++ resources/views/panel/layouts/app.blade.php
@@ -30,6 +30,9 @@
 
   <!-- Template Main CSS File -->
   <link href="{{url('')}}/assets/css/style.css" rel="stylesheet">
+  <meta name="theme-color" content="#6777ef"/>
+<link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
    @yield('css') 
 </head>
 
@@ -57,6 +60,22 @@
 
 <!-- Template Main JS File -->
 <script src="{{url('')}}/assets/js/main.js"></script>
+<script>
+  if ("serviceWorker" in navigator) {
+     // Register a service worker hosted at the root of the
+     // site using the default scope.
+     navigator.serviceWorker.register("/sw.js").then(
+     (registration) => {
+        console.log("Service worker registration succeeded:", registration);
+     },
+     (error) => {
+        console.error(`Service worker registration failed: ${error}`);
+     },
+   );
+ } else {
+    console.error("Service workers are not supported.");
+ }
+</script>
 @yield('script')
 </body>
 
